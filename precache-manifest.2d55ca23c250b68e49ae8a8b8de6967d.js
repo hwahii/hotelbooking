b5e5284bc4650ed14bb2bbd781810132749fba4c
Aaron <aaron@AppledeMacBook-Pro.local>
2019-08-26 01:44:13 +0800

    Updates

---
+++ precache-manifest.2d55ca23c250b68e49ae8a8b8de6967d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a6ca636d3e126d239baf3e4619971c",
+    "url": "/hotelbooking/index.html"
+  },
+  {
+    "revision": "a906c7fc967342aecd84",
+    "url": "/hotelbooking/static/css/main.8c7de42b.chunk.css"
+  },
+  {
+    "revision": "f26473dc1d9906d4fa2d",
+    "url": "/hotelbooking/static/js/2.fd0ce160.chunk.js"
+  },
+  {
+    "revision": "a906c7fc967342aecd84",
+    "url": "/hotelbooking/static/js/main.ec98c4cd.chunk.js"
+  },
+  {
+    "revision": "6edcf09ad7d0b493bb8e",
+    "url": "/hotelbooking/static/js/runtime~main.7a135ad5.js"
+  },
+  {
+    "revision": "54b9a58e252bb00a41e5604d909b039b",
+    "url": "/hotelbooking/static/media/image1.54b9a58e.png"
+  }
+]);
